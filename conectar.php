d7aa76950fbe1e658d5234822267c98878e57a64
danielmestre98 <42392997+danielmestre98@users.noreply.github.com>
2019-09-04 13:56:12 -0300

    Conectar bd teste

--- conectar.php
+++ conectar.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$usuario = "root";
-$senha = "";
-$bd = "academia";
+$usuario = "id10764884_academia";
+$senha = "avip123";
+$bd = "id10764884_academia";
 
 $conn = new mysqli($host, $usuario, $senha, $bd);
 if($conn->connect_errno)
