2c1ea5157cae470c18c9b579020c3289231ae016
danielmestre98 <42392997+danielmestre98@users.noreply.github.com>
2019-09-18 11:42:32 -0300

    Agendamento externo pronto (falta checagem email repetido)

---
+++ lib/salvar_ag_exp.php
@@ -0,0 +1,45 @@
+<?php
+// Conexão com o banco de dados
+include( '../conectar.php' );
+//Recupera os dados dos campos
+$nome = $_POST[ 'nome' ];
+$dia = $_POST[ 'dia' ];
+$hora = $_POST[ 'hora' ];
+$telefone = $_POST['numero'];
+$treinamento = $_POST['treinamento'];
+$email = $_POST['email'];
+$filial = $_POST['filial'];
+$hora = $_POST['hora'];
+$data = explode( "/", $dia );
+
+list( $dia, $mes, $ano ) = $data;
+
+$data = "$ano-$mes-$dia";
+$sql = "INSERT INTO agendamento (data, status, horario, tipo, idFilial, descricaoCancelamento)
+		VALUES ('$data', 'Aguardando aprovação', '$hora', 'Aula Experimental', '$filial', '');";
+$sql2 = "INSERT INTO agendamentoaulaexp (data, email, IdFilial, modalidadeTreinamento, nome, telefone, horario) VALUES ('$data', '$email', '$filial', '$treinamento', '$nome', '$telefone', '$hora')";
+
+
+if ($conn->query($sql) === TRUE) {
+
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+mysqli_close( $conn );
+include ('../conectar.php');
+if ($conn->query($sql2) === TRUE) {
+	header( 'location: ../index' );
+} else {
+    echo "Error: " . $sql2 . "<br>" . $conn->error;
+}
+
+
+
+	
+
+
+
+// Se houver mensagens de erro, exibe-as
+
+
+?>
