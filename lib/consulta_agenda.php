6dbf0b7b640ea162fac5c9a12339ca716da4c1fd
danielmestre98 <42392997+danielmestre98@users.noreply.github.com>
2019-09-28 13:17:04 -0300

    Sync com site

--- lib/consulta_agenda.php
+++ lib/consulta_agenda.php
@@ -1,17 +1,15 @@
 <?php 
 require('../conectar.php');
 session_start();
-if ($_SESSION['tipoPessoa'] == 2){
+
 	$cpf = $_SESSION['filial'];
 	$data = [];
 	$sql = "SELECT dia, evento, horario, f.cidade, f.rua, f.numero, f.estado, f.bairro, p.nome, filial, a.id FROM agenda a INNER JOIN filial f ON (filial = IdFilial) INNER JOIN pessoa p ON (cpffunc = cpf) WHERE filial = '$cpf'";
 	$result = $conn->query($sql);
-}
-else{
+
 $data = [];
 $sql = "SELECT dia, evento, horario, f.cidade, f.rua, f.numero, f.estado, f.bairro, p.nome, filial, a.id FROM agenda a INNER JOIN filial f ON (filial = IdFilial) INNER JOIN pessoa p ON (cpffunc = cpf)";
 $result = $conn->query($sql);
-}
 
 while($row = $result->fetch_array(MYSQLI_ASSOC)){
 	$row['horario'] = date("H:i", strtotime($row['horario']));
