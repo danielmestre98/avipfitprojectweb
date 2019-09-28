a678e0116e8f220c64ea91cdf1c965e46723f643
danielmestre98 <42392997+danielmestre98@users.noreply.github.com>
2019-09-28 16:38:02 -0300

    Retirado * dos horários de alunos

--- admin/novo_aluno.php
+++ admin/novo_aluno.php
@@ -114,7 +114,7 @@ include_once( 'nav.php' );
 						?>
 					</select>
 				</div>
-				<b><red>*</red> Preencha o horário aos dias da semana em que o aluno frequentará o studio.</b>
+				Preencha o horário aos dias da semana em que o aluno frequentará o studio.
 				<div class="form-row">
 					<div class="form-group col-md-1">
 						<label for="segunda">Segunda</label>
