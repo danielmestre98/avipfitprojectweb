72489098cc15ca7b42f41e8d3e155becf09f3e73
danielmestre98 <42392997+danielmestre98@users.noreply.github.com>
2019-10-04 08:10:49 -0300

    Acento no email

--- testeemail.php
+++ testeemail.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: text/html; charset=UTF-8');
 require_once('phpmailer/class.phpmailer.php');
  include('phpmailer/class.smtp.php'); 
 $mailer = new PHPMailer();
@@ -8,7 +9,7 @@ $mailer->IsHTML(true);
 $mailer->Port = 587; //Indica a porta de conexão 
 $mailer->Host = 'email-ssl.com.br';//Endereço do Host do SMTP 
 $mailer->SMTPAuth = true; //define se haverá ou não autenticação 
-$mailer->CharSet = 'iso-8859-1';
+$mailer->CharSet = 'UTF-8';
 $mailer->Username = 'no-reply@avipfit.com'; //Login de autenticação do SMTP
 $mailer->Password = 'Noreply@123'; //Senha de autenticação do SMTP
 $mailer->FromName = 'AVIPfit - No reply'; //Nome que será exibido
