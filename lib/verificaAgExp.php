a3333c5135b584c53986b0bea3403c7c7b7d35bc
danielmestre98 <42392997+danielmestre98@users.noreply.github.com>
2019-10-17 19:20:21 -0300

    Bug agendamento aula experimental

--- lib/verificaAgExp.php
+++ lib/verificaAgExp.php
@@ -3,8 +3,7 @@
         
     if(isset($_POST['email'])) {
         $email = $_POST['email'];
-
-		$hora = date ("h:i:s", strtotime($_POST['hora']));
+		$hora = $_POST['hora'];
 		$filial = $_POST['filial'];
 		$dia = $_POST['dia'];
 		$data = explode( "/", $dia );
@@ -14,7 +13,6 @@
 		$data = "$ano-$mes-$dia";
 		
 		$sql = "SELECT * FROM agendamentoaulaexp WHERE email = '$email' AND data = '$data' AND IdFilial = '$filial' AND horario = '$hora'";
-
         $query = $conn->query($sql);
 		$num_rows = $query->num_rows;
         if( $num_rows > 0 ){
