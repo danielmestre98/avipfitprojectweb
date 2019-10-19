9917d4e6ad84db5c0cbe2b11086af1fb66c44143
danielmestre98 <42392997+danielmestre98@users.noreply.github.com>
2019-10-19 18:36:25 -0300

    Ajustes datas em depoimentos e ajustes em colab

--- colab/depoimentos.php
+++ colab/depoimentos.php
@@ -25,31 +25,19 @@ include_once( 'nav.php' );
 		<br>
 		<h5>Clique em gerenciar depoimentos para aprovar novos registros.</h5>
 		<br>
-		<?php 
-		include('../conectar.php');
-		$sql = "SELECT * FROM depoimentos d INNER JOIN pessoa p ON (d.cpf = p.cpf) WHERE d.status = 'Pendente'";
-		$result = mysqli_query($conn, $sql) or die ( mysqli_error($conn));
-		if ( mysqli_num_rows( $result ) >= 1 ) {
-		
-		?>
-			<a class="btn btn-primary" href="novos_dep">Gerenciar depoimentos <span class="badge badge-light">Novos</span></a>
-		<?php }
-		else{ ?>
-			<a class="btn btn-primary" href="novos_dep">Gerenciar depoimentos</a>
-		<?php }?>
-			<br>
-		<br>
-			<br>
 			<?php
 			require( '../conectar.php' );
-			$sql2 = "SELECT foto, p.nome, descricao FROM depoimentos d INNER JOIN pessoa p ON (d.cpf = p.cpf) WHERE d.status = 'Aprovado'";
+			$sql2 = "SELECT foto, p.nome, descricao, data FROM depoimentos d INNER JOIN pessoa p ON (d.cpf = p.cpf) WHERE d.status = 'Aprovado' ORDER BY data ASC";
 			$result = mysqli_query( $conn, $sql2 )or die( mysqli_error( $conn ) );
 			if ( mysqli_num_rows( $result ) < 1 ){
 				echo "Não existem depoimentos registrados.";
 			}
 			while ( $row = mysqli_fetch_array( $result ) ) {
+				$data = explode("-", $row['data']);
+				list($ano, $mes, $dia) = $data;
+				$data = "$dia/$mes/$ano";
 			?>
-				<p><img src="../fotos/<?=$row['foto']?>" width="70" height="70" alt=""> <h3><?=$row['nome']?></h3></p>
+				<p><img src="../fotos/<?=$row['foto']?>" width="70" height="70" alt=""> <h3><?=$row['nome']?> - <?=$data?></h3></p>
 				<p><?=$row['descricao']?></p>
 				<br><br>
 				
--- depoimentos.php
+++ depoimentos.php
@@ -14,24 +14,30 @@ include_once( 'nav.php' );
 		} );
 	} );
 </script>
+
 <body>
 	<main class="page-content pt-2">
 		<div id="overlay" class="overlay"></div>
 		<div class="container-fluid p-5">
-			<p><h1>Depoimentos</h1></p>
-			<br>
-			<h5>Confira os depoimentos dos alunos AVIPfit!</h5>
-			<br>
+			<p>
+				<h1>Depoimentos</h1>
+			</p>
+		<br>
+		<h5>Clique em gerenciar depoimentos para aprovar novos registros.</h5>
+		<br>
 			<?php
 			require( 'conectar.php' );
-			$sql2 = "SELECT foto, p.nome, descricao FROM depoimentos d INNER JOIN pessoa p ON (d.cpf = p.cpf) WHERE d.status = 'Aprovado'";
+			$sql2 = "SELECT foto, p.nome, descricao, data FROM depoimentos d INNER JOIN pessoa p ON (d.cpf = p.cpf) WHERE d.status = 'Aprovado' ORDER BY data ASC";
 			$result = mysqli_query( $conn, $sql2 )or die( mysqli_error( $conn ) );
-			if ( mysqli_num_rows( $result ) == 0 ){
+			if ( mysqli_num_rows( $result ) < 1 ){
 				echo "Não existem depoimentos registrados.";
 			}
 			while ( $row = mysqli_fetch_array( $result ) ) {
+				$data = explode("-", $row['data']);
+				list($ano, $mes, $dia) = $data;
+				$data = "$dia/$mes/$ano";
 			?>
-				<p><img src="../fotos/<?=$row['foto']?>" width="70" height="70" alt=""> <h3><?=$row['nome']?></h3></p>
+				<p><img src="fotos/<?=$row['foto']?>" width="70" height="70" alt=""> <h3><?=$row['nome']?> - <?=$data?></h3></p>
 				<p><?=$row['descricao']?></p>
 				<br><br>
 				
@@ -43,7 +49,6 @@ include_once( 'nav.php' );
 			mysqli_close( $conn );
 
 			?>
-
 
 
 		</div>
