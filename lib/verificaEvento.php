76928766433f970c9de53363c5e649619ec62511
danielmestre98 <42392997+danielmestre98@users.noreply.github.com>
2019-10-20 16:19:32 -0300

    Ajustes agenda de eventos hora inicio hora fim

--- lib/verificaEvento.php
+++ lib/verificaEvento.php
@@ -10,6 +10,8 @@
 		$filial = $_SESSION['filial'];
 		$sql2 = "SELECT * FROM agenda WHERE '$hora' BETWEEN horario AND horafim AND dia = '$dia' AND filial = '$filial'";
 		$sql = "SELECT * FROM agenda WHERE '$horafim' BETWEEN horario AND horafim AND filial = '$filial' AND dia = '$dia'";
+		$sql3 = "SELECT * FROM agenda WHERE '$hora' = horario AND filial = '$filial' AND dia = '$dia'";
+		$sql4 = "SELECT * FROM agenda WHERE '$horafim' = fim AND filial = '$filial' AND dia = '$dia'";
         $query = $conn->query($sql);
 		
 		$num_rows = $query->num_rows;
@@ -18,7 +20,14 @@
 		include('../conectar.php');
 		$query = $conn->query($sql2);
 		$num_rows += $query->num_rows;
-
+		mysqli_close($conn);
+		include('../conectar.php');
+		$query = $conn->query($sql3);
+		$num_rows += $query->num_rows;
+		mysqli_close($conn);
+		include('../conectar.php');
+		$query = $conn->query($sql4);
+		$num_rows += $query->num_rows;
         if( $num_rows > 0 ){
            	echo "false";
         } else {
