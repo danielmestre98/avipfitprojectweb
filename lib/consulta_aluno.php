67213d8ae2c3ea1bf95040193aa45d757c78e0da
danielmestre98 <42392997+danielmestre98@users.noreply.github.com>
2019-10-21 08:10:56 -0300

    Adicionado frequencia na grid de pesquisa de aluno

--- lib/consulta_aluno.php
+++ lib/consulta_aluno.php
@@ -7,7 +7,47 @@ $result = $conn->query($sql) or die(mysqli_error($conn));
 
 
 while($row = $result->fetch_array(MYSQLI_ASSOC)){
+	if (!empty($row['segunda'])){
+		if(!empty($row['terca']) or !empty($row['quarta']) or !empty($row['quinta']) or !empty($row['sexta']) or !empty($row['sabado'])){
+		$row['segunda'] = "Segunda ".$row['segunda'].", ";
+		}else{
+			$row['segunda'] = "Segunda ".$row['segunda'];
+		}
+	}
+	if (!empty($row['terca'])){
+		if(!empty($row['quarta']) or !empty($row['quinta']) or !empty($row['sexta']) or !empty($row['sabado'])){
+		$row['terca'] = "Terça ".$row['terca'].", ";
+		}else{
+			$row['terca'] = "Terça ".$row['terca'];
+		}
+	}
+	if (!empty($row['quarta'])){
+		if(!empty($row['quinta']) or !empty($row['sexta']) or !empty($row['sabado'])){
+		$row['quarta'] = "Quarta ".$row['quarta'].", ";
+		}else{
+			$row['quarta'] = "Quarta ".$row['quarta'];
+		}
+		
+	}
+	if (!empty($row['quinta'])){
+		if(!empty($row['sexta']) or !empty($row['sabado'])){
+		$row['quinta'] = "Quinta ".$row['quinta'].", ";
+		}else{
+			$row['quinta'] = "Quinta ".$row['quinta'];
+		}
+	}
+	if (!empty($row['sexta'])){
+		if(!empty($row['sabado'])){
+		$row['sexta'] = "Sexta ".$row['sexta'].", ";
+		}else{
+			$row['sexta'] = "Sexta ".$row['sexta'];
+		}
+	}
+	if (!empty($row['sabado'])){
+		$row['sabado'] = "Sábado ".$row['sabado'];
+	}
   $data[] = $row;
+	
 }
 
 
