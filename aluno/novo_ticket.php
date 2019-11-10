2a925e7348b34cebf13511141ea576725ee80fe1
danielmestre98 <42392997+danielmestre98@users.noreply.github.com>
2019-11-10 19:29:16 -0300

    Ajuste tickets e avaliação para aluno

--- aluno/novo_ticket.php
+++ aluno/novo_ticket.php
@@ -1,96 +1,76 @@
 <?php
-// Conexão com o banco de dados
-include( '../conectar.php' );
-date_default_timezone_set( 'America/Sao_Paulo' );
-//Recupera os dados dos campos
-$foto = $_FILES[ "foto" ];
-$descricao = addslashes( $_POST[ 'desc' ] );
-$titulo = addslashes( $_POST[ 'nome_ticket' ] );
-$agora = date( 'Y-m-d H:i:s' );
-
-// Se a foto estiver sido selecionada
-if ( !empty( $foto[ "name" ] ) ) {
-
-	// Tamanho máximo do arquivo em bytes
-
-
-	$error = array();
-
-
-	// Verifica se o tamanho da imagem é maior que o tamanho permitido
-
-	// Se não houver nenhum erro
-	if ( count( $error ) == 0 ) {
-
-		// Pega extensão da imagem
-		preg_match( "/\.(gif|bmp|png|jpg|jpeg){1}$/i", $foto[ "name" ], $ext );
-
-		// Gera um nome único para a imagem
-		$nome_imagem = md5( uniqid( time() ) ) . "." . $ext[ 1 ];
-
-		// Caminho de onde ficará a imagem
-		$caminho_imagem = "../tickets/" . $nome_imagem;
-
-		// Faz o upload da imagem para seu respectivo caminho
-		move_uploaded_file( $foto[ "tmp_name" ], $caminho_imagem );
-	}
-}else{
-	$foto = "";
-}
-session_start();
-$email2 = $_SESSION[ 'email' ];
-$ip = $_SERVER[ 'REMOTE_ADDR' ];
-$data = date( 'Y-m-d H:i:s' );
-$datacad = date( 'Y-m-d' );
-$cpf = $_SESSION[ 'cpf' ];
-// Insere os dados no banco
-$sql = "INSERT INTO ticket (titulo, classificacao, status, prioridade, usuario)
-		VALUES ('$titulo', 'Não classificado', 'Aberto', 'Não classificado', '$cpf');";
-include( '../conectar.php' );
-if ( $conn->query( $sql ) === TRUE ) {
-	$id = mysqli_insert_id( $conn );
-} else {
-	echo "Error: " . $sql . "<br>" . $conn->error;
-}
-mysqli_close( $conn );
-
-$sql2 = "INSERT INTO ticketRespostas (ticket, descricao, imagem, datahora, tipo) VALUES ('$id', '$descricao', '$nome_imagem', '$agora', 'User'); ";
-
-include( '../conectar.php' );
-if ( $conn->query( $sql2 ) === TRUE ) {
-
-} else {
-	echo "Error: " . $sql2 . "<br>" . $conn->error;
-}
-mysqli_close( $conn );
-
-include( '../conectar.php' );
-$sql = addslashes($sql);
-$sql2 = addslashes($sql2);
-
-
-$log = "INSERT INTO log (ip, data, tabela, usuario, codigo) VALUES ('$ip', '$data', 'pessoa, cliente, horario, realiza, mensalidade', '$email2', '$sql $sql2')";
-
-if ( $conn->query( $log ) === TRUE ) {
-	session_start();
-	if ( $_SESSION[ 'tipoPessoa' ] == '1' ) {
-		header( 'location: ../admin/tickets' );
-	} else {
-		if($_SESSION['tipoPessoa'] == '2'){
-			header( 'location: ../colab/tickets' );
-		}else{
-			if($_SESSION['tipoPessoa'] == '3'){
-				header('location: ../aluno/tickets');
-			}
-		}
-	}
-
-} else {
-	echo "Error: " . $log . "<br>" . $conn->error;
-}
-mysqli_close( $conn );
-
-// Se houver mensagens de erro, exibe-as
-
+include_once( 'nav.php' );
 
 ?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>AVIPfit - Ticket</title>
+	<link rel="stylesheet" href="../css/reddot.css">
+</head>
+<script>
+	jQuery( function ( $ ) {
+		$( document ).ready( function () {
+			$( "#ajuda" ).addClass( "active" );
+			$( "#ajuda_drop" ).slideDown( 200 );
+			$( "#tickets" ).addClass( "bg-dark active" )
+		} );
+	} );
+</script>
+
+<body>
+	<main class="page-content pt-2">
+		<div id="overlay" class="overlay"></div>
+		<div class="container-fluid p-5">
+			<h1>Novo ticket</h1>
+			<br>
+			
+			<form id="new_ticket" action="../lib/novo_ticket" enctype="multipart/form-data" method="post">
+				<div class="form-row">
+					<div class="form-group col-md-12">
+						<label for="nome_ticket">
+							<red>*</red>Título</label>
+					
+
+						<input type="text" name="nome_ticket" required class="form-control" id="nome_ticket" placeholder="Insira um título ao ticket">
+					</div>
+				</div>
+				<div class="form-row">
+					<div class="form-group col-md-12">
+						<label for="desc"><red>*</red>Descrição</label>
+						<textarea name="desc" maxlength="1022" class="form-control" id="desc" cols="30" required placeholder="Descreva detalhadamente sobre o ticket" rows="10"></textarea>
+					</div>
+				</div>
+				<div class="form-row">
+					<div class="form-group col-md-12">
+						<label for="cidade">Anexar arquivos (Formatos: jpg, jpeg, png)</label>
+						<input type="file" name="foto" class="form-control-file" id="foto">
+					</div>
+				</div>
+				<p>Campos com <red>*</red> são obrigatórios.</p>
+
+
+				<a class="btn btn-primary" href="tickets">Voltar</a>
+				<button type="submit" class="btn btn-primary float-right">Enviar</button>
+			</form>
+
+
+
+
+
+
+
+		</div>
+	</main>
+	<!-- page-content" -->
+	</div>
+
+
+	<script src="../js/jquery.mask.js"></script>
+
+	<script src="../js/jquery.validate.min.js"></script>
+	<script src="../js/additional-methods.min.js"></script>
+	<script src="../js/valida_form.js"></script>
+</body>
+</html>
