d13e91f9b5993171e9bef01bddc67f5480d61a13
danielmestre98 <42392997+danielmestre98@users.noreply.github.com>
2019-11-10 20:30:55 -0300

    Verificar evento ajustado

--- lib/verificaEvento.php
+++ lib/verificaEvento.php
@@ -8,10 +8,10 @@
 		$horafim = $_POST['horafim'];
 		session_start();
 		$filial = $_SESSION['filial'];
-		$sql2 = "SELECT * FROM agenda WHERE '$hora' BETWEEN horario AND horafim OR horario BETWEEN '$hora' AND '$horafim' AND dia = '$dia' AND filial = '$filial'";
-		$sql = "SELECT * FROM agenda WHERE '$horafim' BETWEEN horario AND horafim OR horafim BETWEEN '$hora' AND '$horafim' AND filial = '$filial' AND dia = '$dia'";
-		$sql3 = "SELECT * FROM agenda WHERE '$hora' = horario OR '$hora' = horafim AND filial = '$filial' AND dia = '$dia'";
-		$sql4 = "SELECT * FROM agenda WHERE '$horafim' = horafim OR '$horafim' = horario AND filial = '$filial' AND dia = '$dia'";
+		$sql2 = "SELECT * FROM agenda WHERE ('$hora' BETWEEN horario AND horafim OR horario BETWEEN '$hora' AND '$horafim') AND dia = '$dia' AND filial = '$filial'";
+		$sql = "SELECT * FROM agenda WHERE ('$horafim' BETWEEN horario AND horafim OR horafim BETWEEN '$hora' AND '$horafim') AND filial = '$filial' AND dia = '$dia'";
+		$sql3 = "SELECT * FROM agenda WHERE ('$hora' = horario OR '$hora' = horafim) AND filial = '$filial' AND dia = '$dia'";
+		$sql4 = "SELECT * FROM agenda WHERE ('$horafim' = horafim OR '$horafim' = horario) AND filial = '$filial' AND dia = '$dia'";
         $query = $conn->query($sql);
 		
 		$num_rows = $query->num_rows;
